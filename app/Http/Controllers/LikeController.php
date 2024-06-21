b326771ef72abb6d5c456fc05627b1d2fe523f6a
dsarrion <samidas@msn.com>
2024-06-21 13:07:02 +0200

    new functions

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Like;
+use App\Models\Track;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -20,19 +22,20 @@ class LikeController extends Controller
             'user_id' => 'required|numeric',
             'track_id' => 'required|numeric'
         ];
-        $validator = Validator::make($request->input(),$rules);
-        if($validator->fails()){
+        $validator = Validator::make($request->input(), $rules);
+        if ($validator->fails()) {
             return response()->json([
                 'status' => false,
                 'errors' => $validator->errors()->all()
-            ],400);
+            ], 400);
         }
         $like = new Like($request->input());
         $like->save();
         return response()->json([
             'status' => true,
-            'message' => 'Like GUARDADO correctamente'
-        ],200);
+            'message' => 'Like GUARDADO correctamente',
+            'data' => $like
+        ], 200);
     }
 
     public function show(Like $like)
@@ -46,6 +49,41 @@ class LikeController extends Controller
         return response()->json([
             'status' => true,
             'message' => 'Like BORRADO correctamente'
-        ],200);
+        ], 200);
+    }
+
+    public function hasLike($user_id, $track_id)
+    {
+        //Bucar el like
+        $like = Like::where('user_id', $user_id)->where('track_id', $track_id)->first();
+
+        //Si hay like
+        if($like){
+            return response()->json([
+                'hasLike' => true,
+                'like' => $like
+            ]);
+        }
+        
+        //Si no hay like
+        return response()->json(['hasLike' => $like !== null]);
+    }
+
+    public function videosWithLikesByUser($user_id)
+    {
+        // Obtener el usuario
+        $user = User::find($user_id);
+
+        if (!$user) {
+            return response()->json(['message' => 'Usuario no encontrado'], 404);
+        }
+
+        // Obtener los tracks que tienen likes del usuario
+        $tracks = Track::whereHas('likes', function ($query) use ($user_id) {
+            $query->where('user_id', $user_id);
+        })->get();
+
+        // Devolver la respuesta
+        return response()->json(['tracks' => $tracks]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -40,7 +40,6 @@ Route::resource('categories',CategoryController::class);
 Route::resource('comments',CommentController::class);
 Route::get('comments/by-video/{videoId}', [CommentController::class, 'getCommentsByVideo']);
 
-Route::resource('likes',LikeController::class);
 Route::post('user/upload/avatar', [UserController::class, 'uploadAvatar']);
 Route::get('avatar/{filename}', [UserController::class, 'getImage']);
 
@@ -49,5 +48,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('user/details/{id}', [UserController::class, 'detail']);
     Route::put('user/update', [AuthController::class, 'update']); 
     Route::get('user/avatar/{filename}', [UserController::class, 'getImage']);
+    Route::resource('likes',LikeController::class);
+    Route::get('likes/user/{user_id}/track/{track_id}', [LikeController::class, 'hasLike']);
+    Route::get('tracks/likes/user/{user_id}', [LikeController::class, 'videosWithLikesByUser']);
 
 });
